e9cfab18717f5a031ad69530b3ede2f6ba4df4b7
Luca <james.lol2000@gmail.com>
2024-04-09 14:57:01 +0200

    add color and coverage

---
+++ src/color.rs
@@ -0,0 +1,27 @@
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Color {
+    White,
+    Black,
+}
+
+impl Color {
+    fn inverse(&self) -> Self {
+        match self {
+            Self::White => Self::Black,
+            Self::Black => Self::White,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Color;
+
+    #[test]
+    fn inverse() {
+        let white = Color::White;
+        assert!(white.inverse() == Color::Black);
+        let black = Color::Black;
+        assert!(black.inverse() == Color::White);
+    }
+}
